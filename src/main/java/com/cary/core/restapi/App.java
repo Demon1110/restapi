60c8a6438082281028b10f27adafe360b43b4019
wxy <852232096@qq.com>
2024-11-26 16:53:21 +0800

    修改调试

--- src/main/java/com/cary/core/restapi/App.java
+++ src/main/java/com/cary/core/restapi/App.java
@@ -3,11 +3,21 @@ package com.cary.core.restapi;
 import org.noear.solon.Solon;
 import org.noear.solon.annotation.SolonMain;
 import org.noear.solon.scheduling.annotation.EnableScheduling;
+import org.noear.solon.core.event.EventListener;
+import org.noear.solon.core.AppContext;
 
 @SolonMain
 @EnableScheduling
 public class App {
     public static void main(String[] args) {
-        Solon.start(App.class, args);
+        Solon.start(App.class, args, app -> {
+            // 添加应用启动完成的监听器
+            app.onEvent(AppContext.class, new EventListener<AppContext>() {
+                @Override
+                public void onEvent(AppContext context) {
+                    System.out.println("应用启动完成！");
+                }
+            });
+        });
     }
 }
